01df11383aa4079996123ba737b66299bad2ba3e
ramyakathirvelu0301 <ramya.kathirvelu1991@gmail.com>
2026-07-21 18:53:37 +0530

    Add files via upload

---
+++ day_02/gradeCalculation.js
@@ -0,0 +1,20 @@
+function gradeCalculation(marks){
+    //let marks=85;
+    switch(true)
+    {
+        case(marks>=90):
+          console.log("A Grade");
+          break;
+        case(marks>=80 && marks<90):
+          console.log("B Grade");
+          break;
+        case(marks>=35 && marks<80):
+          console.log("C Grade");
+          break;
+        default:
+            console.log("D Grade");
+
+
+    }
+}
+gradeCalculation(80);
---
+++ day_02/numberType.js
@@ -0,0 +1,14 @@
+function numberType(){
+    let number=-10;
+    if (number>0)
+    {
+        console.log("Given number is positive");
+    }else if (number<0){
+        console.log("Given number is negative");
+    }else{
+        console.log("Given number is neutral");
+    }
+
+    }
+
+numberType();
